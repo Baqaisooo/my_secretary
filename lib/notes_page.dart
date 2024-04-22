7217d67fcd081d86538d654dec11c1a153c437b1
baqaisooo <aboali_6438@hotmail.com>
2024-04-22 23:24:59 +0300

    create my_secretary project

---
+++ lib/notes_page.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class NotesPage extends StatelessWidget {
+  const NotesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("NotesPage"),
+      ),
+    );
+  }
+}
